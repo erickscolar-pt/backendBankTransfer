3013c48398c1efeab271061fe4e2b6d0c09ffd6c
Erick Scolar <erick.scolar@outlook.com>
2022-09-13 22:49:12 -0300

    first commit

---
+++ src/main/java/com/banktransfer/TransferenciaBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.banktransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciaBancariaApplication.class, args);
+	}
+
+}
